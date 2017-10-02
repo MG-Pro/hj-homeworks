bf95444b9fac4a386716d5312d5c4d0e5fafcd3e
MG <mg84@bk.ru>
2017-10-02 21:31:31 +0300

    hw1 Task# fixed

--- browser/slider/slider.js
+++ browser/slider/slider.js
@@ -1,17 +1,17 @@
 'use strict';
-document.onreadystatechange = function(){
-  if(document.readyState === 'interactive'){
-    let counter = 0;
-    let images = document.getElementById('store').children;
-    let container = document.getElementById('slider');
-    function slider() {
-      container.setAttribute('src', images[counter].getAttribute('src'));
-      counter++;
-      if(counter >= images.length) {
-        counter = 0;
-      }
-    }
-    slider();
-    setInterval(slider, 5000);
-    console.log(images);
-  }};
+
+let counter = 0;
+let images = document.getElementById('store').children;
+let container = document.getElementById('slider');
+
+function slider() {
+  container.setAttribute('src', images[counter].getAttribute('src'));
+  counter++;
+  if (counter >= images.length) {
+    counter = 0;
+  }
+}
+
+slider();
+setInterval(slider, 5000);
+console.log(images);
