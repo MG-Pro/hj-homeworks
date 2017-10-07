aa1bd7defd9f7624266efcdcd17c6fde603daddc
MG <mg84@bk.ru>
2017-10-08 00:51:36 +0300

    hw3 Task#1 keyevent cmpl

--- event-object/piano/piano.js
+++ event-object/piano/piano.js
@@ -1,1 +1,31 @@
 'use strict';
+window.onload = function () {
+
+  const tracks = [
+    'first.mp3',
+    'second.mp3',
+    'third.mp3',
+    'fourth.mp3',
+    'fifth.mp3'
+  ];
+  const piano = document.getElementsByClassName('set')[0];
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Alt') {
+      piano.classList.remove('middle', 'lower', 'higher');
+      piano.classList.add('lower');
+    } else if (e.key === 'Shift') {
+      piano.classList.remove('middle', 'lower', 'higher');
+      piano.classList.add('higher');
+    }
+  });
+
+  document.addEventListener('keyup', function (e) {
+    if (e.key === 'Alt' || e.key === 'Shift') {
+      piano.classList.remove('middle', 'lower', 'higher');
+      piano.classList.add('middle');
+    }
+  });
+
+};
+
