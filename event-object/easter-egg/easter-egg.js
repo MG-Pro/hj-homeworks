4de3c7bb64406b4e01f717ef74428cac5fda3cba
MG <mg84@bk.ru>
2017-10-08 19:36:39 +0300

    hw3 Task#3 cmpl

--- event-object/easter-egg/easter-egg.js
+++ event-object/easter-egg/easter-egg.js
@@ -1,25 +1,30 @@
 'use strict';
 window.onload = function () {
-
   const keyCodes = [17, 18, 84];
   const secretWord = [89, 84, 78, 74, 75, 74, 85, 66, 90];
-
-  let secretPressed = {};
   let pressed = {};
+  let navVision = false;
 
   document.addEventListener('keydown', function (e) {
     pressed[e.keyCode] = true;
-    for (let code of keyCodes) {
-      if (!pressed[code])
-        return;
+    if (!navVision) {
+      for (let code of keyCodes) {
+        if (!pressed[code])
+          return;
+      }
+      document.getElementsByTagName('nav')[0].classList.add('visible');
+      navVision = true;
+    } else {
+      for (let code of secretWord) {
+        if (!pressed[code])
+          return;
+      }
+      document.getElementsByClassName('secret')[0].classList.add('visible');
     }
     pressed = {};
-    document.getElementsByTagName('nav')[0].classList.add('visible');
-
   });
 
   document.addEventListener('click', function (e) {
     delete pressed[e.keyCode];
   });
-
 };
