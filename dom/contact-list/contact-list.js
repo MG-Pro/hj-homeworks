266103350922c902b20aea3cddb49520e48b6bcc
MG <mg84@bk.ru>
2017-10-10 23:51:09 +0300

    hw4 Task#1 cmpl

---
+++ dom/contact-list/contact-list.js
@@ -0,0 +1,25 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', function (e) {
+
+  const listElem = document.querySelector('.contacts-list');
+  listElem.removeChild(document.querySelector('.contacts-list li'));
+
+  try {
+    const contacts = JSON.parse(loadContacts());
+    contacts.forEach((val) => {
+      let li = document.createElement('li');
+      let strong = document.createElement('strong');
+      li.dataset.email = val.email;
+      li.dataset.phone = val.phone;
+      strong.innerHTML = val.name;
+      listElem.appendChild(li);
+      li.appendChild(strong);
+    });
+  } catch (e) {
+    console.log(new Error(e));
+  }
+});
+
+
+
+
