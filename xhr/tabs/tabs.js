b7954d752d7242fdfd9a236209bc584219efa28b
MG <mg84@bk.ru>
2017-10-12 23:29:57 +0300

    hw5 Task#2 cmpl

---
+++ xhr/tabs/tabs.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('nav a');
+
+  for (let link of links) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      let preloader = document.getElementById('preloader');
+      preloader.classList.remove('hidden');
+      let xhr = new XMLHttpRequest();
+      xhr.open(
+        'GET',
+        this.href,
+        true);
+      xhr.send();
+      xhr.addEventListener('load', function () {
+        if(xhr.status !== 200) {
+          console.log(new Error('Ошибка!'));
+          return;
+        }
+        preloader.classList.add('hidden');
+        document.getElementById('content').innerHTML = xhr.responseText;
+        link.classList.add('active');
+      });
+      for (let link of links) {
+        link.classList.remove('active');
+      }
+    });
+  }
+
+
+
+});
