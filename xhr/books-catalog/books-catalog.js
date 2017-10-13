c98cac66ce7b6f34fc708b4b69547dfa12fbd620
MG <mg84@bk.ru>
2017-10-13 21:31:41 +0300

    hw5 Task#3 in work

--- xhr/books-catalog/books-catalog.js
+++ xhr/books-catalog/books-catalog.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#content').removeChild(document.querySelector('#content li'));
+
+  let xhr = new XMLHttpRequest();
+  xhr.open(
+    'GET',
+    'http://netology-fbb-store-api.herokuapp.com/book/'
+  );
+  xhr.send();
+  xhr.addEventListener('load', function (e) {
+    if(xhr.status !== 200) {
+      console.log(new Error('Ошибка!'));
+      return;
+    }
+    console.log(xhr.response);
+  })
 
 });
