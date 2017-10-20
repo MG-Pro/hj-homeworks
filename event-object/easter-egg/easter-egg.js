64371b14077ff58fac402971f36ca98548f694a2
mg84 <mg84@bk.ru>
2017-10-20 15:24:52 +0700

    hw3 Task #3 fixed

--- event-object/easter-egg/easter-egg.js
+++ event-object/easter-egg/easter-egg.js
@@ -3,24 +3,22 @@ window.onload = function () {
   const keyCodes = [17, 18, 84];
   const secretWord = [89, 84, 78, 74, 75, 74, 85, 66, 90];
   let pressed = {};
-  let navVision = false;
 
   document.addEventListener('keydown', function (e) {
     pressed[e.keyCode] = true;
-    if (!navVision) {
+
       for (let code of keyCodes) {
         if (!pressed[code])
           return;
       }
       document.getElementsByTagName('nav')[0].classList.add('visible');
-      navVision = true;
-    } else {
+
       for (let code of secretWord) {
         if (!pressed[code])
           return;
       }
       document.getElementsByClassName('secret')[0].classList.add('visible');
-    }
+
     pressed = {};
   });
 
