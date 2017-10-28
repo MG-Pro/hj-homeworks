47a97cad649828ef689d89ebca433b32901114bd
MG <mg84@bk.ru>
2017-10-28 14:12:50 +0300

    hw8 Task#3 'shop'cmpl

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function addHandlers() {
+  const items = document.querySelectorAll('figure');
+  for (let item of items) {
+    item.addEventListener('click', addItemToCard);
+  }
+
+  function addItemToCard(e) {
+    e.preventDefault();
+    addToCart({
+      title: e.target.dataset.title,
+      price: e.target.dataset.price
+    });
+  }
+}
+
+addHandlers();
+
+const showMoreBtn = document.querySelector('.show-more');
+showMoreBtn.addEventListener('click', addHandlers);
