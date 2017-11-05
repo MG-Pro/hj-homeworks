918a8b845b46370d0114613798accb788b0643c7
mg84 <mg84@bk.ru>
2017-11-05 18:40:20 +0300

    hw8 Task #3 'shop' fxd

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,21 +1,13 @@
 'use strict';
 
-function addHandlers() {
-  const items = document.querySelectorAll('figure');
-  for (let item of items) {
-    item.addEventListener('click', addItemToCard);
-  }
+document.querySelector('.items-list').addEventListener('click', addItemToCard);
 
-  function addItemToCard(e) {
-    e.preventDefault();
-    addToCart({
-      title: e.target.dataset.title,
-      price: e.target.dataset.price
-    });
-  }
+function addItemToCard(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('add-to-cart'))
+    return;
+  addToCart({
+    title: e.target.dataset.title,
+    price: e.target.dataset.price
+  });
 }
-
-addHandlers();
-
-const showMoreBtn = document.querySelector('.show-more');
-showMoreBtn.addEventListener('click', addHandlers);
