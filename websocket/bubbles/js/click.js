251289354b100da764a4e352b3d20088f9bfe481
mg84 <mg84@bk.ru>
2017-11-09 12:50:44 +0300

    hw10 Task #1 'bubbles' fxd

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -5,12 +5,10 @@ window.addEventListener('beforeunload', () => {
   ws.onclose = function () {};
   ws.close()
 });
-
+showBubbles(ws);
 document.addEventListener('click', function (e) {
   const click = {};
   click.x = e.pageX;
   click.y = e.pageY;
   ws.send(JSON.stringify(click));
-
-  showBubbles(ws);
 });
