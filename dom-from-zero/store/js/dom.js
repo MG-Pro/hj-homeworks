66bd334b5bdaba9747e47ad02dc1f161b2435e01
MG <mg84@bk.ru>
2017-11-16 20:59:40 +0300

    hw12 Task#3 'store' cmpl

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,15 @@
 'use strict';
+function createElement(node) {
+  const element = document.createElement(node.name);
+  if (typeof node.props === 'object' && node.props !== null) {
+    Object.keys(node.props).forEach(i => element.setAttribute(i, node.props[i]));
+  }
+  node.childs.forEach(child => {
+    if (typeof child === 'string') {
+      element.textContent = child;
+    } else {
+      element.appendChild(createElement(child));
+    }
+  });
+  return element;
+}
