7246125d9698a5108770446edca044c93c30f427
MG <mg84@bk.ru>
2017-11-19 20:10:03 +0300

    hw13 Task# 'socket' in work

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+ws.addEventListener('message', e => {
+  console.log(e);
+});
+
+ws.addEventListener('close', e => {
+  console.log(e);
+});
+
+window.editor.addEventListener('update', e => {
+  const ctx = e.canvas.getContext('2d');
+  const image = ctx.getImageData(0, 0, 100, 100);
+  const binary = Uint8Array.from(image.data);
+  ws.send(binary.buffer)
+});
+
+
