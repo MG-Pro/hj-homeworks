6982e6d2df28928e9e4851fe4f8f21826f606d9a
MG <mg84@bk.ru>
2017-11-22 22:43:10 +0300

    hw14 Task#1 'starSky' cmpl

--- canvas/star-field/js/star.js
+++ canvas/star-field/js/star.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const canvas = document.querySelector('canvas');
-
-
-
+canvas.width = 800;
+canvas.height = 400;
+canvas.addEventListener('click', () => createSky(canvas));
+createSky(canvas);
 
 function createSky(elem) {
   const ctx = elem.getContext('2d');
-  const pi = Math.PI;
-  const cWidht = elem.width;
-  const cHeight = elem.height;
+
+  const cWidht = elem.clientWidth;
+  const cHeight = elem.clientHeight;
+  const count = getRandom(200, 400);
 
   ctx.beginPath();
   ctx.fillStyle = '#000';
@@ -20,18 +22,31 @@ function createSky(elem) {
   ctx.closePath();
   ctx.fill();
 
+  for (let i = 0; i <= count; i++) {
+    createStar(new randomParam(ctx));
+  }
 
+  function randomParam(ctx) {
+    const colors = ['#ffffff', '#ffe9c4', '#d4fbff'];
+    this.color = colors[getRandom(0, 3)];
+    this.x = getRandom(2, cWidht - 2);
+    this.y = getRandom(2, cHeight - 2);
+    this.globalAlpha = getRandom(0.8, 1, 2);
+    this.r = getRandom(0, 1.1 / 2, 2);
+    this.ctx = ctx;
+  }
 
+  function getRandom(min, max, digits = 0) {
+    return parseFloat((Math.random() * (max - min) + min).toFixed(digits));
+  }
 
-  createStar(100, 100, 10, ctx);
-
-
-  function createStar(x, y, r, ctx) {
+  function createStar(param) {
+    const ctx = param.ctx;
     ctx.beginPath();
-    ctx.fillStyle = 'red';
-    ctx.arc(x, y, r, 0, 2 * pi);
+    ctx.fillStyle = param.color;
+    ctx.globalAlpha = param.globalAlpha;
+    ctx.arc(param.x, param.y, param.r, 0, 2 * Math.PI);
     ctx.fill();
   }
 }
 
-createSky(canvas);
