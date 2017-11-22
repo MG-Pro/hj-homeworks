5fa21f8561c6d06d7792a2317f8865f5319cda35
MG <mg84@bk.ru>
2017-11-22 23:45:42 +0300

    hw14 Task#1 'draw' in work

--- canvas/draw/js/draw.js
+++ canvas/draw/js/draw.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const canvas = document.querySelector('#draw');
+
+window.addEventListener('resize', (e) => {
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight;
+});
+
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+
+const ctx = canvas.getContext('2d');
+const d = 20; // dimensions
+canvas.addEventListener('click', (e) => {
+  const point = [e.layerX, e.layerY];
+  ctx.beginPath();
+  if (e.shiftKey) {
+    ctx.arc(...point, d, 0, 2 * Math.PI);
+  } else {
+    ctx.rect(...point, d, d);
+  }
+  ctx.fill();
+});
+
