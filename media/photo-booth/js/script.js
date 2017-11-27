f0debb9f31bee40de6e880b38f9f9131f47c4d15
mg84 <mg84@bk.ru>
2017-11-28 00:53:42 +0300

    hw15 Task #1 'photo-booth' in work

--- media/photo-booth/js/script.js
+++ media/photo-booth/js/script.js
@@ -1,30 +1,42 @@
 'use strict';
-const errorCont = document.querySelector('#error-message');
-errorCont.style.display = 'block';
+
+const vidCont = document.querySelector('.app');
+const errorCont = vidCont.querySelector('#error-message');
+const controls = vidCont.querySelector('.controls');
+const vid = document.createElement('video');
+const canvas = document.createElement('canvas');
 
 if (!navigator.mediaDevices) {
   errorCont.textContent = 'API not support';
 }
 
-
-
 navigator.mediaDevices
   .getUserMedia({video: true, audio: false})
   .then(stream => {
-    console.log(stream);
-    const vidCont = document.querySelector('.app');
-    const vid = document.createElement('video');
-    vid.width = '100%';
-    vid.height = '100%';
-    vidCont.appendChild(vid);
+    controls.firstElementChild.addEventListener('click', takePhoto);
+    controls.classList.toggle('visible');
+    vidCont.insertBefore(vid, controls);
+    vid.width = vidCont.clientWidth;
+    vid.height = vidCont.clientHeight;
     vid.src = URL.createObjectURL(stream);
-
-
-
 
 
   })
   .catch(err => {
     errorCont.textContent = 'Нет доступа к камере';
+    errorCont.classList.toggle('visible');
     console.log(err);
   });
+
+function takePhoto(e) {
+  canvas.width = vid.clientWidth;
+  canvas.height = vid.clientHeight;
+  vid.style.display = 'none';
+  vidCont.insertBefore(canvas, controls);
+  canvas.style.display = 'block';
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(vid, 0, 0);
+
+
+
+}
