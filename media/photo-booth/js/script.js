4aafeabdf8c6027ec6e6b1ab3a70e149624746e7
MG <mg84@bk.ru>
2017-11-27 23:02:06 +0300

    hw15 Task#1 'photo-booth' testing

--- media/photo-booth/js/script.js
+++ media/photo-booth/js/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
-if (navigator.MediaDevices) {
+if (!navigator.mediaDevices) {
   console.log(new Error('API not support'));
 }
+
+navigator.mediaDevices
+  .getUserMedia({video: true, audio: false})
+  .then(stream => {
+    console.log(stream);
+    const vidCont = document.querySelector('.app');
+    const vid = document.createElement('video');
+    vid.width = '100%';
+    vid.height = '100%';
+    vidCont.appendChild(vid);
+    vid.src = stream;
+
+
+
+
+
+  })
+  .catch(err => console.log('oh noes'));
