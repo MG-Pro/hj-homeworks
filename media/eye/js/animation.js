79b475482b799ea80e204e4bb533bd522aa5986d
MG <mg84@bk.ru>
2017-11-29 00:13:47 +0300

    hw15 Task# 'eye' in work

--- media/eye/js/animation.js
+++ media/eye/js/animation.js
@@ -1,1 +1,14 @@
 'use strict';
+
+document.addEventListener('mousemove', e => {
+  const eye = document.querySelector('.big-book__pupil');
+  let pupilX;
+  let pupilY;
+  let pupilSize;
+
+  eye.style.setProperty('--pupil-x', pupilX);
+  eye.style.setProperty('--pupil-y', pupilY);
+  eye.style.setProperty('--pupil-size', pupilSize);
+
+
+});
