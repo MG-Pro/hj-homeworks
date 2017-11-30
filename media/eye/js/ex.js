4704fe6ade3879f50044da96c8d0fb86e46839f5
MG <mg84@bk.ru>
2017-11-30 18:08:07 +0300

    hw15 Task #2 'eyeh' in work add alt decision

---
+++ media/eye/js/ex.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const eye = document.querySelector('.big-book__pupil');
+const eyeX = eye.getBoundingClientRect().x;
+const eyeY = eye.getBoundingClientRect().y;
+const height = window.innerHeight;
+const width = window.innerWidth;
+
+const corners = [
+  {
+    x: 0,
+    y: 0
+  },
+  {
+    x: width,
+    y: 0
+  },
+  {
+    x: width,
+    y: height
+  },
+  {
+    x: 0,
+    y: height
+  }
+];
+
+const maxDist = corners.reduce(toMaxDist, 0);
+
+function dist(aX, aY, bX, bY) {
+  return Math.sqrt((aX - bX) * (aX - bX) + (aY - bY) * (aY - bY))
+}
+
+function toMaxDist(max, cur) {
+  const distance = dist(cur.x, cur.y, eyeX, eyeY);
+  if (distance > max) {
+    return distance
+  } else {
+    return max
+  }
+}
+
+document.addEventListener('mousemove', function (e) {
+  const cursorRelCoord = {
+    x: e.clientX - eyeX,
+    y: e.clientY - eyeY
+  };
+  const cursorAngle = Math.atan(cursorRelCoord.y / cursorRelCoord.x);
+  const cursorPolarCoord = {
+    r: dist(eyeX, eyeY, e.clientX, e.clientY) / maxDist,
+    t: cursorAngle
+  };
+  
+//  let pupilSize = 1;
+//  if (cursorPolarCoord.r * 3 < 3 && cursorPolarCoord.r * 3 > 1) {
+//    pupilSize = 1 / cursorPolarCoord.r
+//  } else if (cursorPolarCoord.r * 3 <= 1) {
+//    pupilSize = 3
+//  }
+  
+  let pupilX;
+  let pupilY;
+  
+  if (cursorRelCoord.x > 0) {
+    pupilX = 30 * cursorPolarCoord.r * Math.cos(cursorPolarCoord.t);
+  } else {
+    pupilX = -(30 * cursorPolarCoord.r * Math.cos(cursorPolarCoord.t));
+  }
+  
+  if (cursorRelCoord.x > 0) {
+    pupilY = 30 * cursorPolarCoord.r * Math.sin(cursorPolarCoord.t);
+  } else {
+    pupilY = -(30 * cursorPolarCoord.r * Math.sin(cursorPolarCoord.t));
+  }
+  
+  
+  eye.style.setProperty('--pupil-size', 1);
+  eye.style.setProperty('--pupil-x', pupilX + 'px');
+  eye.style.setProperty('--pupil-y', pupilY + 'px');
+});
